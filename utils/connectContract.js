1e19e7da7744d564d075b17cd0071f15fa33461e
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-08-19 18:05:21 +0100

    suprememods

---
+++ utils/connectContract.js
@@ -0,0 +1,26 @@
+import abiJSON from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+    //Note: Your contractAddress will start with 0x, delete everything between the quotes and paste your contract address.
+    const contractAddress = "0x2c50dd449b8F8d78A9d34AeEE342CF95dff698F3";
+    const contractABI = abiJSON.abi;
+    let rsvpContract;
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        //checking for eth object in the window
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log("ERROR:", error);
+    }
+    return rsvpContract;
+  }
+  
+  export default connectContract;
